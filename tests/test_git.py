c85cbe38e9f8a5a8613901c4a59ebfe48f128897
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:59:31 -0400

    Refactor import statements in test_git.py to handle different module paths.

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,13 +1,20 @@
-import os
 import unittest
-from custom_nodes.git import CreateRepoNode
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+try:
+    from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
+except:
+    from ..git import CreateRepoNode
+
+
 
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
-        os.chdir("E:/git/ComfyUI/")
         node = CreateRepoNode()
         node.create_repo(subdirectory="my_repo")
         # Add assertions or additional test logic here
+
 
 if __name__ == "__main__":
     unittest.main()
