e765c77f63902362fa249db992ca3453b108dc72
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:27:53 -0400

    Added a new class `PublicGitRepo` with a method `validate_git_repo_url` that parses a git URL using `giturlparse`.

--- git.py
+++ git.py
@@ -1,1 +1,23 @@
-pip install gitpython
+import giturlparse
+
+class PublicGitRepo:
+    """replaces the text in the given text string with a given other text at some key positions"""
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "url": ("STRING", {"multiline": False}),  # Added url input
+                "email": ("STRING", {"multiline": False})  # Added email input
+            }
+        }
+
+    CATEGORY = "text"
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "validate_git_repo_url"
+
+    def validate_git_repo_url(self,**kwargs):
+        git_url = kwargs.get("url", None)
+        parsed_url = giturlparse.parse(git_url)
+
+        return (git_url,)
