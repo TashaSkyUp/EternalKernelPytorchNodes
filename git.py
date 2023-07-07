e6215518e344f266c30cfa6d59d0c98b9d99c3fc
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:16:08 -0400

    Added a commented out class `PromptTemplate` with a method `prompt_template_handler` that replaces text in a given string with another text at some key positions.y

--- git.py
+++ git.py
@@ -1,8 +1,23 @@
-import os
-
-class Node:
-    def __init__(self):
-        pass
-
-    def process(self):
-        raise NotImplementedError("Subclasses must implement the process method")
+#class PromptTemplate:
+#    """replaces the text in the given text string with a given other text at some key positions"""
+#
+#    @classmethod
+#    def INPUT_TYPES(s):
+#        return {"required":
+#            {
+#                "text": ("STRING", {"multiline": True}),
+#                "replacement": ("STRING", {"multiline": True}),
+#                 "key1": ("STRING", {"multiline": False}),
+#             }
+#         }
+#
+#     CATEGORY = "text"
+#     RETURN_TYPES = ("STRING",)
+#     FUNCTION = "prompt_template_handler"
+#
+#     def prompt_template_handler(self, text: str, replacement, key1):
+#         """
+#         >>> PromptTemplate().prompt_template_handler("hello world", "universe", "world")
+#         'hello universe'
+#         """
+#         return (text.replace(key1, replacement),)
