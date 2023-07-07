11fe586b29b66b2a006d9dd9716c52600eb30d49
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:56:19 -0400

    aider: Set the working directory to E:\git\ComfyUI\
    # Context:
    USER: it would work if you would set the working directory to e:\git\ComfyUI\

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,5 +1,9 @@
+import os
 import unittest
 from custom_nodes.git import CreateRepoNode
+
+# Set the working directory to E:\git\ComfyUI\
+os.chdir("E:/git/ComfyUI/")
 
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
