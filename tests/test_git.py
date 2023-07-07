602d5e1ef86d97400143df3edf8d96faa03a63e9
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:59:59 -0400

    aider: Fixed relative import error in test_git.py.
    # Context:
    USER: I ran this command:
    
     python E:/git/ComfyUI/custom_nodes/EternalKernelLiteGraphNodes/tests/test_git.py
    
    And got this output:
    
    Total VRAM 10239 MB, total RAM 98225 MB
    Set vram state to: NORMAL_VRAM
    Device: cuda:0 NVIDIA GeForce RTX 3080
    ETK> Failed to import comfy.samplers, assuming no comfyui skipping SD related nodes
    Using sub quadratic optimization for cross attention, if you have memory or speed issues try using: --use-split-cross-attention
    Using sub quadratic optimization for cross attention, if you have memory or speed issues try using: --use-split-cross-attention
    ETK> advanced clip not found, skipping it
    +--------------------- Traceback (most recent call last) ---------------------+
    | E:\git\ComfyUI\custom_nodes\EternalKernelLiteGraphNodes\tests\test_git.py:6 |
    | in <module>                                                                 |
    |                                                                             |
    |    3 import os                                                              |
    |    4 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.pat |
    |    5 try:                                                                   |
    | >  6     from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRep |
    |    7 except:                                                                |
    |    8     from ..git import CreateRepoNode                                   |
    |    9                                                                        |
    |                                                                             |
    | E:\git\ComfyUI\custom_nodes\EternalKernelLiteGraphNodes\git.py:1 in         |
    | <module>                                                                    |
    |                                                                             |
    | >  1 import giturlparse                                                     |
    |    2 import git                                                             |
    |    3                                                                        |
    |    4                                                                        |
    +-----------------------------------------------------------------------------+
    ModuleNotFoundError: No module named 'giturlparse'
    
    During handling of the above exception, another exception occurred:
    
    +--------------------- Traceback (most recent call last) ---------------------+
    | E:\git\ComfyUI\custom_nodes\EternalKernelLiteGraphNodes\tests\test_git.py:8 |
    | in <module>                                                                 |
    |                                                                             |
    |    5 try:                                                                   |
    |    6     from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRep |
    |    7 except:                                                                |
    | >  8     from ..git import CreateRepoNode                                   |
    |    9                                                                        |
    |   10                                                                        |
    |   11                                                                        |
    +-----------------------------------------------------------------------------+
    ImportError: attempted relative import with no known parent package

--- tests/test_git.py
+++ tests/test_git.py
@@ -3,18 +3,15 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 try:
-    from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
-except:
-    from ..git import CreateRepoNode
-
-
+    from custom_nodes.git import CreateRepoNode
+except ImportError:
+    from custom_nodes.git import CreateRepoNode
 
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
         node = CreateRepoNode()
         node.create_repo(subdirectory="my_repo")
         # Add assertions or additional test logic here
-
 
 if __name__ == "__main__":
     unittest.main()
