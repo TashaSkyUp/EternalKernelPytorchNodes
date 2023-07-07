611fce2b2f22ae1951f3fdd4a5b846d7d4d8b221
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:53:53 -0400

    aider: Fixed relative import error in test_git.py.
    # Context:
    USER: I ran this command:
    
     python E:/git/ComfyUI/custom_nodes/EternalKernelLiteGraphNodes/tests/test_git.py
    
    And got this output:
    
    Traceback (most recent call last):
      File "E:\git\ComfyUI\custom_nodes\EternalKernelLiteGraphNodes\tests\test_git.py", line 3, in <module>
        from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
    ModuleNotFoundError: No module named 'custom_nodes.EternalKernelLiteGraphNodes'
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "E:\git\ComfyUI\custom_nodes\EternalKernelLiteGraphNodes\tests\test_git.py", line 5, in <module>
        from ..git import CreateRepoNode
    ImportError: attempted relative import with no known parent package

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,17 +1,11 @@
 import unittest
-try:
-    from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
-except:
-    from ..git import CreateRepoNode
-
-
+from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
 
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
         node = CreateRepoNode()
         node.create_repo(subdirectory="my_repo")
         # Add assertions or additional test logic here
-
 
 if __name__ == "__main__":
     unittest.main()
