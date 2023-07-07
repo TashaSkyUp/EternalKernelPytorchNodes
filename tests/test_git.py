e633aefeed6bbbc9ebf9e1a47315bf84ce6493e4
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:37:31 -0400

    Added a new test case for CreateRepoNode.

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,11 +1,13 @@
 import unittest
 from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
 
+
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
         node = CreateRepoNode()
         node.create_repo(subdirectory="my_repo")
         # Add assertions or additional test logic here
+
 
 if __name__ == "__main__":
     unittest.main()
