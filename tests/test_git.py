74d394cc8d96657e5686c193ec288557ea63e539
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-07-07 02:36:55 -0400

    Refactored import statement in test_git.py to use the CreateRepoNode from the custom_nodes.EternalKernelLiteGraphNodes.git module.

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,5 +1,5 @@
 import unittest
-from git import CreateRepoNode
+from custom_nodes.EternalKernelLiteGraphNodes.git import CreateRepoNode
 
 class TestCreateRepoNode(unittest.TestCase):
     def test_create_repo(self):
