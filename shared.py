4ad95e0405744c3f233550fb61b4c79285aa95f8
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-10-28 13:32:33 -0400

    GDE_PATH added

--- shared.py
+++ shared.py
@@ -3,3 +3,5 @@ import os
 # state that is shared, available to all modules
 
 ETK_PATH = os.path.dirname(os.path.abspath(__file__))
+# this is two levels down
+GDE_PATH = os.path.normpath(os.path.join(ETK_PATH, "../../"))
