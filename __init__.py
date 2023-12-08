939c0a0f44beda10fdce7d09a5183ed58be07471
Tasha Upchurch <TashaSkyUp@gmail.com>
2023-12-08 11:14:11 -0500

    changes to adhere to comfy-ui dyanamic web extensions

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 import importlib
-
+WEB_DIRECTORY = "./web"
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 OLD_NODE_CLASS_MAPPINGS = {
     "Interpolation": "LatentInterpolation",
     "Image Pad To Match": "PadToMatch",
